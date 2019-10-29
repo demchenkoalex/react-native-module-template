8c9866cd173fe4aeb0ae7fb2276196494eeaf5af
Alex Demchenko <alexdemchenko@yahoo.com>
2019-10-29 23:22:12 +0100

    Add example

---
+++ example/src/App.tsx
@@ -0,0 +1,8 @@
+import Counter from 'library-template'
+import React from 'react'
+
+const App = () => {
+  return <Counter />
+}
+
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,18 @@
-import React, { useState } from 'react'
+import * as React from 'react'
 import { Button, Text, View } from 'react-native'
 
 const Counter = () => {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = React.useState(0)
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: 200,
+      }}
+    >
       <Text>You pressed {count} times</Text>
       <Button onPress={() => setCount(count + 1)} title='Press Me'></Button>
     </View>
