ef1f9020893ec1bf2a91b233ff7608ef3edd7539
Alex Demchenko <alexdemchenko@yahoo.com>
2019-10-29 22:59:06 +0100

    Add react and react-native

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+import { Button, Text, View } from 'react-native'
+
+const Counter = () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>You pressed {count} times</Text>
+      <Button onPress={() => setCount(count + 1)} title='Press Me'></Button>
+    </View>
+  )
+}
+
+export default Counter
