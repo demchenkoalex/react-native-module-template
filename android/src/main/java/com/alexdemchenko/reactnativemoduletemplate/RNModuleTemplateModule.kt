a90d43a1a3cb9f0828fc5a76f4ea50ee4733bd18
Alex Demchenko <alexdemchenko@yahoo.com>
2021-03-21 19:35:08 +0100

    Update dependencies

--- android/src/main/java/com/alexdemchenko/reactnativemoduletemplate/RNModuleTemplateModule.kt
+++ android/src/main/java/com/alexdemchenko/reactnativemoduletemplate/RNModuleTemplateModule.kt
@@ -5,7 +5,7 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule
 
 class RNModuleTemplateModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
 
-    override fun getName() = "RNModuleTemplate"
+    override fun getName() = "RNModuleTemplateModule"
 
     override fun getConstants(): MutableMap<String, Any> {
         return hashMapOf("count" to 1)
--- react-native-module-template.podspec
+++ react-native-module-template.podspec
@@ -10,11 +10,10 @@ Pod::Spec.new do |s|
 
   s.authors      = package['author']
   s.homepage     = package['homepage']
-  s.platform     = :ios, "10.0"
+  s.platform     = :ios, "11.0"
 
   s.source       = { :git => "https://github.com/demchenkoalex/react-native-module-template.git", :tag => "v#{s.version}" }
   s.source_files  = "ios/**/*.{h,m,swift}"
 
   s.dependency 'React'
 end
-
